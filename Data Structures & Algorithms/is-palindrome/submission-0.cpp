7e254ae9937c6c20e2c81279529bfa54dc895223
Mradul Pal <mradulpal@outlook.com>
2026-05-04 08:35:03 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int i = 0, j = s.length();
+        while (i <= j) {
+            if (isalnum(s[i]) && isalnum(s[j])) {
+                if (tolower(s[i]) != tolower(s[j])) {
+                    return false;
+                } 
+                i++;
+                j--;
+            } else if (!isalnum(s[i])) {
+                i++;
+            } else if (!isalnum(s[j])) {
+                j--;
+            }
+        }
+        return true;
+    }
+};
