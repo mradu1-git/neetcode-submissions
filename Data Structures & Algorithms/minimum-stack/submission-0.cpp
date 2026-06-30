97cb1c3cf539475aa843598027097b1a46c3d344
Mradul Pal <mradulpal@outlook.com>
2026-06-30 08:01:12 +0530

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.cpp
@@ -0,0 +1,26 @@
+class MinStack {
+private:
+    stack<int> st;
+    stack<int> minSt;
+public:
+    MinStack() {}
+    
+    void push(int val) {
+        st.push(val);
+        val = min(val, minSt.empty() ? val : minSt.top());
+        minSt.push(val);
+    }
+    
+    void pop() {
+        st.pop();
+        minSt.pop();
+    }
+    
+    int top() {
+        return st.top();
+    }
+    
+    int getMin() {
+        return minSt.top();
+    }
+};
